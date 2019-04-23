01629f59cef9dccaee5267b833ff33ef30f9b55a
Einar Persson <me@einarpersson.com>
2019-04-23 16:34:51 +0200

    feat(app): 🎸 List connected clients in Home Screen (#88)
    * feat: 🎸 Make it possible to get all consents for user
    
    The app now has a service that gets all consents for the current
    account. Jest e2e test is included. Also adds a
    getAndApproveConsentRequest convenience function for the app e2e-server.
    
    * refactor: 💡 Convert HomeScreen to func component with hooks
    
    * refactor: 💡 Just call the function

--- __e2e__/src/phone.js
+++ __e2e__/src/phone.js
@@ -33,6 +33,10 @@ export const getConsentRequest = (url) => {
 
 export const approveConsentRequest = consents.approve
 
+export const getAndApproveConsentRequest = (url) => getConsentRequest(url).then(approveConsentRequest)
+
+export const getAllConsents = consents.getAll
+
 export async function setConfig (config) {
   Object.entries(config).forEach(([key, val]) =>
     Object.assign(Config, { [key]: val }))
