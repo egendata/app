2084c4340c49f3f379efd52b6c3d027bf7a9210f
Einar Persson <me@einarpersson.com>
2019-06-03 10:20:13 +0200

    Chore/make e2e great again (#103)
    * e2e tests are now working for the new flow.

--- __e2e__/src/server.js
+++ __e2e__/src/server.js
@@ -14,12 +14,6 @@ if (!process.env.OPERATOR_URL) {
 const app = express()
 app.use(json())
 app.post('/:method', async ({ body = {}, params: { method } }, res, next) => {
-  if (body.args) {
-    console.info(`CALL: ${method} with args`, body.args)
-  } else {
-    console.info(`CALL: ${method}`)
-  }
-
   try {
     const result = await phone[method](body.args)
     res.send(result)
@@ -30,7 +24,7 @@ app.post('/:method', async ({ body = {}, params: { method } }, res, next) => {
 
 app.use((error, req, res, _) => {
   const { status, message, stack } = error
-  console.error(error)
+  console.error(`${status}: ${message}`)
   res.status(status || 500).send({ message, stack })
 })
 
