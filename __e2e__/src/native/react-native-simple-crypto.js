1efffcf80cacae811321986e50f24a82e740b817
Radu Achim <radu.achim@iteam.se>
2019-11-14 10:08:08 +0100

    style: 💄 run eslint on them

--- __e2e__/src/native/react-native-simple-crypto.js
+++ __e2e__/src/native/react-native-simple-crypto.js
@@ -23,7 +23,7 @@ export const RSA = {
           } else {
             resolve({ private: privateKey, public: publicKey })
           }
-        }
+        },
       )
     })
   },
@@ -31,24 +31,24 @@ export const RSA = {
     return Promise.resolve(
       createSign(hash)
         .update(data)
-        .sign(privateKey, 'base64')
+        .sign(privateKey, 'base64'),
     )
   },
   verify: (secretToVerify, data, key, hash) => {
     return Promise.resolve(
       createVerify(hash)
         .update(secretToVerify)
-        .verify(key, data, 'base64')
+        .verify(key, data, 'base64'),
     )
   },
   encrypt: (data, key) => {
     return Promise.resolve(
-      publicEncrypt(key, Buffer.from(data)).toString('base64')
+      publicEncrypt(key, Buffer.from(data)).toString('base64'),
     )
   },
   decrypt: (rsaEncryptedMessage, key) => {
     return Promise.resolve(
-      privateDecrypt(key, Buffer.from(rsaEncryptedMessage)).toString('utf8')
+      privateDecrypt(key, Buffer.from(rsaEncryptedMessage)).toString('utf8'),
     )
   },
 }
@@ -58,14 +58,14 @@ export const SHA = {
     return Promise.resolve(
       createHash('SHA256')
         .update(text)
-        .digest('base64')
+        .digest('base64'),
     )
   },
   sha512: text => {
     return Promise.resolve(
       createHash('SHA512')
         .update(text)
-        .digest('base64')
+        .digest('base64'),
     )
   },
 }
