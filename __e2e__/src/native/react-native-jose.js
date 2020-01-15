6e8ca0729e9145aa5a0ec002b992044266389bcc
Radu Achim <radu.achim@iteam.se>
2020-01-15 15:41:09 +0100

    test: 💍 mock for react-native-jose 0.5.0

--- __e2e__/src/native/react-native-jose.js
+++ __e2e__/src/native/react-native-jose.js
@@ -1,9 +1,6 @@
 import * as jwt from 'jwt-lite'
 
-const Jose = {
-  sign: (payload, keys, header) => jwt.sign(payload, keys.jwk, header),
-  verify: (token, jwk) => jwt.verify(token, jwk),
-  decode: (token, options) => jwt.decode(token, options),
-}
-
-export default Jose
+export const sign = (payload, keys, header) =>
+  jwt.sign(payload, keys.jwk, header)
+export const verify = (token, jwk) => jwt.verify(token, jwk)
+export const decode = (token, options) => jwt.decode(token, options)
